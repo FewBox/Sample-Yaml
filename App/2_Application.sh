053d526749d0ef5f04fd45c01472a5b1b56edfd0
landpy <landpy@outlook.com>
2019-03-30 15:45:24 +0800

    Remove .

--- App/2_Application.sh
+++ App/2_Application.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 NS=fewbox-staging
-APP=sample
 # Service
-. ./Utility/Service.sh $NS $APP unsplash-service
-. ./Utility/Service.sh $NS $APP auth-service
-. ./Utility/Service.sh $NS $APP photo-service
-. ./Utility/Service.sh $NS $APP review-service
-. ./Utility/Service.sh $NS $APP rating-service
+. ./Utility/Service.sh $NS unsplash unsplash-service
+. ./Utility/Service.sh $NS auth auth-service
+. ./Utility/Service.sh $NS photo photo-service
+. ./Utility/Service.sh $NS review review-service
+. ./Utility/Service.sh $NS rating rating-service
 # Deployment
-. ./Utility/Deployment.sh $NS $APP latest unsplash-deployment fewbox/sample-istio-unsplashapp:latest 80
-. ./Utility/Deployment.sh $NS $APP latest auth-deployment fewbox/sample-istio-auth:latest 80
-. ./Utility/Deployment.sh $NS $APP latest photo-deployment fewbox/sample-istio-photo:latest 80
-. ./Utility/Deployment.sh $NS $APP latest review-deployment fewbox/sample-istio-review:latest 80
-. ./Utility/Deployment.sh $NS $APP latest rating-deployment fewbox/sample-istio-rating:latest 80
+. ./Utility/Deployment.sh $NS unsplash latest unsplash-deployment fewbox/sample-istio-unsplashapp:latest 80
+. ./Utility/Deployment.sh $NS auth latest auth-deployment fewbox/sample-istio-auth:latest 80
+. ./Utility/Deployment.sh $NS photo latest photo-deployment fewbox/sample-istio-photo:latest 80
+. ./Utility/Deployment.sh $NS review latest review-deployment fewbox/sample-istio-review:latest 80
+. ./Utility/Deployment.sh $NS rating latest rating-deployment fewbox/sample-istio-rating:latest 80
