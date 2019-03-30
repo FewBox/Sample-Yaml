edc430bac3f05077d2d8f01041f77f59f150db32
landpy <landpy@outlook.com>
2019-03-30 14:24:29 +0800

    Fix bug.

--- App/2_Application.sh
+++ App/2_Application.sh
@@ -9,7 +9,7 @@ APP=sample
 . ./Utility/Service.sh $NS $APP rating-service
 # Deployment
 . ./Utility/Deployment.sh $NS $APP latest unsplash-deployment fewbox/sample-istio-unsplashapp:latest 80
-. ./Utility/Deployment.sh $NS $APP latest unsplash-deployment fewbox/sample-istio-auth:latest 80
-. ./Utility/Deployment.sh $NS $APP latest unsplash-deployment fewbox/sample-istio-photo:latest 80
-. ./Utility/Deployment.sh $NS $APP latest unsplash-deployment fewbox/sample-istio-review:latest 80
-. ./Utility/Deployment.sh $NS $APP latest unsplash-deployment fewbox/sample-istio-rating:latest 80
+. ./Utility/Deployment.sh $NS $APP latest auth-deployment fewbox/sample-istio-auth:latest 80
+. ./Utility/Deployment.sh $NS $APP latest photo-deployment fewbox/sample-istio-photo:latest 80
+. ./Utility/Deployment.sh $NS $APP latest review-deployment fewbox/sample-istio-review:latest 80
+. ./Utility/Deployment.sh $NS $APP latest rating-deployment fewbox/sample-istio-rating:latest 80
