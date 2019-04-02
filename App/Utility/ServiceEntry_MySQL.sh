8db8f7b8a4400604bd56b0944ec594a1778d5284
Peter Pang <mv@live.cn>
2019-04-02 14:57:49 +0800

    Fix shell script current dir bug.

--- App/Utility/ServiceEntry_MySQL.sh
+++ App/Utility/ServiceEntry_MySQL.sh
@@ -2,4 +2,6 @@
 # $1 - Namespace Name
 # $2 - ServiceEntry Name
 # $3 - Host
-. ./ServiceEntry.sh $1 $2 $3 0.0.0.0/0 tcp 3306 TCP
+
+CURRENTDIR=$(dirname -- "$BASH_SOURCE")
+. $CURRENTDIR/ServiceEntry.sh $1 $2 $3 0.0.0.0/0 tcp 3306 TCP
--- App/Utility/ServiceEntry_RestfulAPI.sh
+++ App/Utility/ServiceEntry_RestfulAPI.sh
@@ -2,5 +2,6 @@
 # $1 - Namespace Name
 # $2 - ServiceEntry Name
 # $3 - Host
-BASEDIR=$(dirname $0)
-. $BASEDIR/ServiceEntry.sh $1 $2 $3 0.0.0.0/0 http 80 HTTP
+
+CURRENTDIR=$(dirname -- "$BASH_SOURCE")
+. $CURRENTDIR/ServiceEntry.sh $1 $2 $3 0.0.0.0/0 http 80 HTTP
