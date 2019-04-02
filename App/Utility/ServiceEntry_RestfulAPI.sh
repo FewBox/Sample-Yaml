ea17c4883fbde529eec84d3690bcea0dc463174b
Peter Pang <mv@live.cn>
2019-04-02 14:22:40 +0800

    Fix bug.

--- App/Utility/ServiceEntry_RestfulAPI.sh
+++ App/Utility/ServiceEntry_RestfulAPI.sh
@@ -2,4 +2,5 @@
 # $1 - Namespace Name
 # $2 - ServiceEntry Name
 # $3 - Host
-. ./ServiceEntry.sh $1 $2 $3 0.0.0.0/0 http 80 HTTP
+BASEDIR=$(dirname $0)
+. $BASEDIR/ServiceEntry.sh $1 $2 $3 0.0.0.0/0 http 80 HTTP
